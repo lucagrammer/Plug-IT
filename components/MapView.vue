b674ae3ecfa41a9e738363603bf14b3328782a37
Luca Minotti <luca2.minotti@mail.polimi.it>
2021-05-02 10:02:57 +0200

    Addition of the MapView component
    MapView component provides a Google Map view

---
+++ components/MapView.vue
@@ -0,0 +1,102 @@
+<!--
+  Component: MapView
+  Description: A Google map view component. Based on the Google My Maps.
+  Named slots: 
+  └── default slot: what to display next to the map view
+  Props:
+  └── map-id: the Google My Map identifier. By default is set to the Plug-IT HQ map
+-->
+
+<template>
+  <section class="map-view-container">
+    <!-- Map view  -->
+    <div class="map-container">
+      <iframe
+        title="Google Map"
+        class="map"
+        :src="`https://www.google.com/maps/d/embed?mid=${mapId}`"
+      ></iframe>
+    </div>
+
+    <!-- Description slot -->
+    <div class="address-container">
+      <address>
+        <slot> </slot>
+      </address>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    // map-id: the Google My Map identifier. By default is set to the Plug-IT HQ map
+    mapId: { type: String, default: () => '1xRytj_13i84wCH0axdS4uMCM_YuJKiJ4' },
+  },
+}
+</script>
+
+<style scoped>
+/* Containers components */
+.map-view-container {
+  margin-top: 1.5em;
+  margin-bottom: 1.5em;
+  width: 100%;
+  display: flow-root;
+}
+.map-container,
+.address-container {
+  position: relative;
+  width: 100%;
+}
+
+/* Additional spacing between address and map */
+address {
+  font-style: normal;
+  padding-left: 2em;
+}
+
+/* Prevent huge margin top space, as the whole component is already spaced apart */
+address :first-child {
+  margin-top: 0px;
+}
+
+/* Map layout */
+.map {
+  width: 100%;
+  border-radius: 20px;
+  height: 290px;
+  border: none;
+}
+
+.map-container * {
+  display: block;
+}
+
+/* Grid management for mobile devices */
+@media (min-width: 726px) {
+  .map-container,
+  .address-container {
+    float: left;
+  }
+  .address-container {
+    width: 30%;
+  }
+  .map-container {
+    width: 70%;
+  }
+}
+/* Grid management for desktop devices */
+@media (max-width: 725px) {
+  .map {
+    max-height: 70vh;
+  }
+  address {
+    padding-left: 0px;
+  }
+  /* Revert the margin for the first element */
+  address :first-child {
+    margin-top: revert;
+  }
+}
+</style>
