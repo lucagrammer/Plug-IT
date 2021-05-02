8a2b697e7a3c239b58d0c3f4543a91bab8ec3f6c
Francesco Ratti <francesco.ratti98@gmail.com>
2021-05-02 13:10:19 +0200

    Added more APIs and "restructured" existing ones.

--- server/rest/api.js
+++ server/rest/api.js
@@ -16,25 +16,48 @@ async function init() {
     Service,
     Message,
    } = db._tables
+
+  // _____________EVENTS_________________
   // API to get all the Events
+  // USED IN EVENTS PAGE
   app.get('/events', async (req, res) => {
     const events = await Event.findAll()
     return res.json(events)
   })
-  // API to get a event by ID.
+  
+  // NOTE: ALL THE EVENTS IN EVENTS PAGE CAN BE RETRIEVED BY RETRIEVING AREA, SO THE FILTER WILL BE POPULATED AND EVENTS ARE SHOWN;
+  // ALTERNATIVE IS FILTERING CLIENT SIDE.
+  // ANTOHER ALTERNATIVE IS:
+  // API to get all events for an area.
+  // USED IN EVENTS PAGE
+  app.get('/event/:area', async (req, res) => {
+    const { area } = req.params
+    const event = await Event.findAll({
+      where: {
+      area_name: area
+    }
+  })
+    return res.json(service)
+  })
+    // API to get a event by ID.
+  //USED IN EVENT PAGE
   app.get('/event/:id', async (req, res) => {
     const { id } = req.params
-    const event = await Event.findByPk(id)
+    const event = await Event.findByPk(id, {include: Person})
     return res.json(event)
   })
 
+  //_____________AREAS________________________
   //API to get all areas, needed to populate the dropdown menu/area introductory page
+  //USED IN DROPDOWN MENU/AREA INTRODUCTORY
   app.get('/areas', async (req, res) => {
     const areas = await Area.findAll()
     return res.json(areas)
   })
 
-    //API to get an area - will get all events by navigation, so no need to explicitly fetch events by area. Event, Service and Person are eagerly fetched to populate the page
+    //API to get an area - will get all events by navigation, so no need to explicitly fetch events by area. Event, Service and Person are eagerly fetched to populate the Area page
+    //USED IN AREA PAGE
+    //USED IN AREA TEAM PAGE (which requires Person to be included)
     app.get('/area/:name', async (req, res) => {
       const { name } = req.params
       const area = await Area.findAll ({include:  [Event, Service, Person]})
@@ -42,40 +65,53 @@ async function init() {
     })
 
 
-/*
-    // API to get all events for an area.
-    app.get('/event/:area', async (req, res) => {
-      const { area } = req.params
-      const event = await Event.findOne({
-        where: { id },
-      })
-      return res.json(event)
-    })
-  */
-
+  //_______________PEOPLE_______________________
   // API to get all the people
+  //USED IN PEOPLE PAGE
   app.get('/people', async (req, res) => {
     const people = await Person.findAll()
     return res.json(people)
   })
+
   // API to get a person by ID.
+  //USED IN PERSON PAGE
   app.get('/person/:id', async (req, res) => {
     const { id } = req.params
-    const person = await Person.findByPk(id)
+    const person = await Person.findByPk(id, {include: Service})
     return res.json(person)
   })
+
+  //_________________SERVICES____________________
   // API to get all the services
+  //USED IN OUT SERVICES PAGE
   app.get('/services', async (req, res) => {
     const services = await Service.findAll()
     return res.json(services)
   })
-  // API to get a service by ID.
-  app.get('/service/:id', async (req, res) => {
-    const { id } = req.params
-    const service = await Service.findByPk(id)
+  //NOTE: ALL THE SERVICES IN SERVICES PAGE CAN BE RETRIEVED BY RETRIEVING AREA, SO THE FILTER WILL BE POPULATED AND SERVICES ARE SHOWN;
+  //ALTERNATIVE IS FILTERING CLIENT SIDE.
+  //ANTOHER ALTERNATIVE IS:
+  //USED IN OUT SERVICES PAGE
+  app.get('/service/:area', async (req, res) => {
+    const { area } = req.params
+    const service = await Service.findAll({
+      where: {
+      area_name: area
+    }
+  })
     return res.json(service)
   })
   
+  // API to get a service by ID.
+  //USED IN Service Page
+  //USED IN SERVICE TEAM PAGE (which requires "Person" to be included)
+  app.get('/service/:id', async (req, res) => {
+    const { id } = req.params
+    const service = await Service.findByPk(id, {include: Person})
+    return res.json(service)
+  })
+
+
 }
 
 init()
