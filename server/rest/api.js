346606cd8677fe77b5b5b6612422a92dce5081a4
Luca Minotti <luca2.minotti@mail.polimi.it>
2021-05-08 10:22:10 +0200

    Added several APIs and updated existing ones

--- server/rest/api.js
+++ server/rest/api.js
@@ -1,121 +1,234 @@
 import express from 'express'
 import initializeDatabase from '../db-conn'
+const { Op, QueryTypes } = require('sequelize')
+const moment = require('moment')
 const app = express()
 
-// We need this one if we send data inside the body as JSON
+// Send data inside the body as JSON
 app.use(express.json())
 
 async function init() {
   // Call the init function that returns the Database
   const db = await initializeDatabase()
-  // Let's extract all the objects we need to perform queries inside the endpoints
+  // Extract all the objects we need to perform queries inside the endpoints
   const { Event, Person, Area, Service, Message } = db._tables
 
-  // _____________EVENTS_________________
-  // API to get all the Events
-  // USED IN EVENTS PAGE
+  // _____________ EVENTS _________________
+  // ALL EVENTS: get all the events
+  // Used in events introductory page
   app.get('/events', async (req, res) => {
-    const events = await Event.findAll()
+    const events = await Event.findAll({
+      attributes: [
+        'id',
+        'icon',
+        'title',
+        'areaName',
+        'date',
+        'time',
+        'overview',
+      ],
+    })
     return res.json(events)
   })
 
-  // NOTE: ALL THE EVENTS IN EVENTS PAGE CAN BE RETRIEVED BY RETRIEVING AREA, SO THE FILTER WILL BE POPULATED AND EVENTS ARE SHOWN;
-  // ALTERNATIVE IS FILTERING CLIENT SIDE.
-  // ANTOHER ALTERNATIVE IS:
-  // API to get all events for an area.
-  // USED IN EVENTS PAGE
-  app.get('/event/:area', async (req, res) => {
-    const { area } = req.params
-    const event = await Event.findAll({
-      where: {
-        area_name: area,
-      },
+  // NEXT 4 EVENTS: get next 4 events
+  // Used in homepage
+  app.get('/upcomingEvents', async (req, res) => {
+    const events = await Event.findAll({
+      attributes: [
+        'id',
+        'icon',
+        'title',
+        'areaName',
+        'date',
+        'time',
+        'overview',
+      ],
+      where: { date: { [Op.gte]: moment().toDate() } },
+      order: [['date', 'ASC']],
+      limit: 4,
     })
-    return res.json(service)
+    return res.json(events)
   })
-  // API to get a event by ID.
-  // USED IN EVENT PAGE
+
+  // EVENT BY ID: get a event by its ID. Include the hosts.
+  // Used in event page
   app.get('/event/:id', async (req, res) => {
     const { id } = req.params
-    const event = await Event.findByPk(id, { include: Person })
+    const event = await Event.findByPk(id, {
+      include: {
+        model: Person, // include the event hosts data
+        attributes: ['id', 'name', 'surname', 'image', 'position'],
+        through: { attributes: [] }, // prevent mapping object from being added
+      },
+      order: [[{ model: Person }, 'surname', 'ASC']],
+    })
     return res.json(event)
   })
 
-  // _____________AREAS________________________
-  // API to get all areas, needed to populate the dropdown menu/area introductory page
-  // USED IN DROPDOWN MENU/AREA INTRODUCTORY
+  // _____________ AREAS _________________
+  // ALL AREAS: get all the areas
+  // Used in area introductory page
   app.get('/areas', async (req, res) => {
-    const areas = await Area.findAll()
+    const areas = await Area.findAll({
+      attributes: ['icon', 'name', 'overview'],
+      order: [['name', 'ASC']],
+    })
     return res.json(areas)
   })
 
-  // API to get an area - will get all events by navigation, so no need to explicitly fetch events by area. Event, Service and Person are eagerly fetched to populate the Area page
-  // USED IN AREA PAGE
-  // USED IN AREA TEAM PAGE (which requires Person to be included)
+  // AREA BY NAME: get an area by its name. Include the services.
+  // Used in area page
   app.get('/area/:name', async (req, res) => {
     const { name } = req.params
     const area = await Area.findByPk(name, {
-      include: [Event, Service, Person],
+      attributes: { exclude: ['name', 'responsible', 'icon'] },
+      include: [
+        {
+          model: Service, // include the services
+          attributes: ['id', 'name', 'icon', 'slogan'],
+        },
+      ],
+      order: [[{ model: Service }, 'name', 'ASC']],
     })
     return res.json(area)
   })
 
-  // _______________PEOPLE_______________________
-  // API to get all the people
-  // USED IN PEOPLE PAGE
+  // _______________ PEOPLE ____________________
+  // ALL PEOPLE: get all the people
+  // Used in people introductory page
   app.get('/people', async (req, res) => {
-    const people = await Person.findAll()
+    const people = await Person.findAll({
+      attributes: ['id', 'name', 'surname', 'image', 'position'],
+      order: [['surname', 'ASC']],
+    })
     return res.json(people)
   })
 
-  // API to get a person by ID.
-  // USED IN PERSON PAGE
-  app.get('/person/:id', async (req, res) => {
+  // PEOPLE BY AREA: get people working in a specified area.
+  // Used in area team page
+  app.get('/areateam/:name', async (req, res) => {
+    const { name } = req.params
+    const data = {}
+
+    // fetch the area responsible
+    const responsibileQuery =
+      'SELECT p.id, p.name, p.surname, p.image, p.position ' +
+      'FROM people AS p, areas AS a ' +
+      'WHERE a.responsible=p.id AND a.name=? '
+    data.area_responsible = await db.query(responsibileQuery, {
+      nest: true,
+      type: QueryTypes.SELECT,
+      replacements: [name],
+    })
+
+    // fetch people working in projects of the specified area
+    const workersQuery =
+      'SELECT p.id, p.name, p.surname, p.image, p.position ' +
+      'FROM people as p ' +
+      'WHERE p.id IN ( ' +
+      ' SELECT DISTINCT person_id FROM assistance as a, services as s1 ' +
+      ' WHERE s1.area_name=:area AND a.service_id=s1.id ' +
+      '  UNION ' +
+      ' SELECT DISTINCT project_manager FROM services as s2 ' +
+      ' WHERE s2.area_name=:area ' +
+      ') ORDER BY p.surname '
+    data.workers = await db.query(workersQuery, {
+      replacements: { area: name },
+      nest: true,
+      type: QueryTypes.SELECT,
+    })
+    return res.json(data)
+  })
+
+  // PEOPLE BY SERVICE: get people working in a specified service.
+  // Used in service team page
+  app.get('/serviceteam/:id', async (req, res) => {
     const { id } = req.params
-    const person = await Person.findByPk(id, { include: Service })
+    const person = await Service.findByPk(id, {
+      attributes: ['name'],
+      include: [
+        {
+          model: Person, // include project manager
+          as: 'pm',
+          attributes: ['id', 'name', 'surname', 'image', 'position'],
+        },
+        {
+          model: Person, // include assistants
+          as: 'assistants',
+          attributes: ['id', 'name', 'surname', 'image', 'position'],
+          through: { attributes: [] }, // prevent mapping object from being added
+        },
+      ],
+      order: [
+        [{ model: Person, as: 'pm' }, 'surname', 'ASC'],
+        [{ model: Person, as: 'assistants' }, 'surname', 'ASC'],
+      ],
+    })
     return res.json(person)
   })
 
-  // _________________SERVICES____________________
-  // API to get all the services
-  // USED IN OUT SERVICES PAGE
+  // PERSON BY ID: get a person by its ID
+  // Used in person page
+  app.get('/person/:id', async (req, res) => {
+    const { id } = req.params
+    const person = await Person.findByPk(id, {
+      include: [
+        {
+          model: Area, // include areas
+          as: 'responsibility',
+          attributes: ['name', 'icon'],
+        },
+        {
+          model: Service, // include pm taks
+          as: 'tasksAsPM',
+          attributes: ['id', 'name', 'icon', 'areaName'],
+        },
+        {
+          model: Service, // include worker tasks
+          as: 'tasksAsAssistant',
+          attributes: ['id', 'name', 'icon', 'areaName'],
+          through: { attributes: [] }, // prevent mapping object from being added
+        },
+      ],
+      order: [
+        [{ model: Service, as: 'tasksAsPM' }, 'name', 'ASC'],
+        [{ model: Service, as: 'tasksAsAssistant' }, 'name', 'ASC'],
+      ],
+    })
+    return res.json(person)
+  })
+
+  // _________________ SERVICES __________________
+  // ALL SERVICES: get all the services
+  // Used in service introductory page
   app.get('/services', async (req, res) => {
-    const services = await Service.findAll()
+    const services = await Service.findAll({
+      attributes: ['id', 'icon', 'name', 'slogan', 'areaName'],
+    })
     return res.json(services)
   })
-  // NOTE: ALL THE SERVICES IN SERVICES PAGE CAN BE RETRIEVED BY RETRIEVING AREA, SO THE FILTER WILL BE POPULATED AND SERVICES ARE SHOWN;
-  // ALTERNATIVE IS FILTERING CLIENT SIDE.
-  // ANTOHER ALTERNATIVE IS:
-  // USED IN OUT SERVICES PAGE
-  app.get('/service/:area', async (req, res) => {
-    const { area } = req.params
-    const service = await Service.findAll({
-      where: {
-        area_name: area,
-      },
+
+  // SERVICE BY ID: get a service by its ID.
+  // Used in service page
+  app.get('/service/:id', async (req, res) => {
+    const { id } = req.params
+    const service = await Service.findByPk(id, {
+      attributes: { exclude: ['id', 'icon', 'project_manager'] },
     })
     return res.json(service)
   })
 
-  // API to get a service by ID.
-  // USED IN Service Page
-  // USED IN SERVICE TEAM PAGE (which requires "Person" to be included)
-  app.get('/service/:id', async (req, res) => {
-    const { id } = req.params
-    const service = await Service.findByPk(id, { include: Person })
-    return res.json(service)
-  })
-
+  // _____________ MESSAGE __________________
+  // ADD A MESSAGE: to add a message
   app.post('/message', async (req, res) => {
-    console.log(req.body)
     const { name, surname, email, subject, message } = req.body
-    console.log(name)
     const messageObj = Message.build({
-      name: name,
-      surname: surname,
-      email: email,
-      subject: subject,
-      message: message,
+      name,
+      surname,
+      email,
+      subject,
+      message,
     })
     await messageObj.save()
     res.sendStatus(200)
