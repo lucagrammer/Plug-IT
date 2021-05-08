c7730a8a8d41814c006bdbf7e78580cde860d476
Luca Leoni <luca3.leoni@mail.polimi.it>
2021-05-08 09:44:21 +0200

    Addition People Pages
    Addition People Pages
    Minor Fixes

--- pages/people/_id.vue
+++ pages/people/_id.vue
@@ -5,47 +5,59 @@
         :default-route="[{ title: 'People', path: '/people' }]"
         :alt-routes="[
           [
-            { title: person.area, path: '/areas/ID' },
-            { title: 'AREANAME Team', path: '/areas/ID/team' },
+            { title: areaName, path: '/areas/' + areaName },
+            {
+              title: areaName + ' Team',
+              path: '/areas/team/' + areaName,
+            },
           ],
           [
-            { title: 'SERVICENAME', path: '/services/ID' },
-            { title: 'SERVICENAME Team', path: '/services/ID/team' },
+            {
+              title: serviceName,
+              path: '/services/' + serviceID,
+            },
+            {
+              title: serviceName + ' Team',
+              path: '/services/team/' + serviceID,
+            },
           ],
           [
             { title: 'Events', path: '/events' },
-            { title: 'EVENTNAME', path: '/events/ID' },
+            {
+              title: eventName,
+              path: '/events/' + eventID,
+            },
           ],
+          [{ title: areaName, path: '/areas/' + areaName }],
         ]"
-        current-page="PERSONNAME"
+        :current-page="name"
       />
-      <div class="position-label">{{ person.position }}</div>
-      <paragraph
-        :image="person.image"
-        :image-cap="person.imageCap"
-        position="right"
-      >
-        <p>{{ person.bio }}</p>
-        <a
-          :href="person.linkedin"
-          target="_blank"
-          rel="noopener"
-          aria-label="personal-linkedin-page"
-          ><base-button
-            icon="mdi mdi-linkedin"
-            label="Find me on Linkedin"
-            @click.native="navigateToExternal('www.linkedin.com')"
-          ></base-button
-        ></a>
+      <div class="position-label">{{ position }}</div>
+
+      <!-- EMPLOYEE BIO AND PHOTO -->
+      <paragraph :image="image" :image-cap="name" position="right">
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div v-html="bio">{{ bio }}</div>
+        <base-button
+          icon="mdi mdi-linkedin"
+          label="Find me on Linkedin"
+          @click.native="
+            navigateToExternal('https://www.linkedin.com/' + linkedin)
+          "
+        ></base-button>
       </paragraph>
     </section>
-    <hr />
-    <section v-if="person.responsibilities" class="section-container">
+    <hr v-if="areaResponsibilities" />
+
+    <!-- AREAS FOR WHICH THE EMPLOYEE IS RESPONSIBLE -->
+    <section v-if="areaResponsibilities" class="section-container">
       <h2>Responsibilities</h2>
-      <grid :elements="person.responsabilities" />
+      <grid :elements="areaResponsibilities" />
     </section>
-    <hr />
-    <section class="section-container">
+    <hr v-if="tasks.length > 0" />
+
+    <!-- SERVICES IN WHICH THE EMPLOYEE WORKS -->
+    <section v-if="tasks.length > 0" class="section-container">
       <h2>Tasks</h2>
       <grid :elements="tasks" />
     </section>
@@ -64,48 +76,82 @@ export default {
     Grid,
   },
   mixins: [RoutingMixins],
-  async asyncData({ $axios, route }) {
-    const { id } = route.params
+  async asyncData({ $axios, route, redirect }) {
+    const personID = route.params.id
+
+    // fetch the employee from the database
     const { data } = await $axios.get(
-      `${process.env.BASE_URL}/api/people/${id}`
+      `${process.env.BASE_URL}/api/person/${personID}`
     )
-    const person = {
-      name: data.name + ' ' + data.surname,
+    if (data === null) {
+      return redirect(
+        '/error?err=The employee you are looking for does not exist.'
+      )
+    }
+
+    // parameters required by the breadcrumb component of the destination page
+    const name = data.name + ' ' + data.surname
+    const fromPeopleToService = '?route=1&person=' + name + '&pID=' + data.id
+    const fromPeopleToArea = '?route=0&person=' + name + '&pID=' + data.id
+
+    // convert the fetched data into the format required by the components
+    const tasks = []
+    let areaResponsibilities
+    data.tasksAsPM.forEach(function (service) {
+      tasks.push({
+        image: service.icon,
+        heading: service.name,
+        destinationLink: '/services/' + service.id + fromPeopleToService,
+        subheading: service.areaName,
+        subheadingLink: '/areas/' + service.areaName + fromPeopleToArea,
+        label: 'Project Manager',
+      })
+    })
+    data.tasksAsAssistant.forEach(function (service) {
+      tasks.push({
+        image: service.icon,
+        heading: service.name,
+        destinationLink: '/services/' + service.id + fromPeopleToService,
+        subheading: service.areaName,
+        subheadingLink: '/areas/' + service.areaName + fromPeopleToArea,
+        label: data.position,
+      })
+    })
+    if (data.responsibility !== null)
+      areaResponsibilities = [
+        {
+          image: data.responsibility.icon,
+          heading: data.responsibility.name,
+          destinationLink:
+            '/areas/' + data.responsibility.name + fromPeopleToArea,
+          label: 'Area Responsible',
+        },
+      ]
+    return {
+      name,
       image: data.image,
       bio: data.bio,
       position: data.position,
       linkedin: data.linkedin,
-      responsibilities:
-        data.responsibility !== undefined
-          ? {
-              image: data.responsibility.image,
-              heading: data.responsibility.name,
-              destinationLink: '/areas/' + data.responsibility.id,
-            }
-          : undefined,
-    }
-    const tasks = []
-    data.services.forEach(function (service) {
-      tasks.push({
-        image: service.image1,
-        heading: service.name,
-        destinationLink: '/services/' + service.id,
-        subheading: service.areaName,
-        subheadingLink: '/areas/' + service.areaName,
-      })
-    })
-    return {
-      person,
+      areaResponsibilities,
       tasks,
     }
   },
   data() {
     return {
-      testCap: 'This is an image',
-      testImage: '/PeopleImages/PhilSchiller.jpg',
+      // Additional query values required by the dynamic breadcrumb
+      areaName: this.$route.query.area,
+      eventName: this.$route.query.event,
+      eventID: this.$route.query.eID,
+      serviceName: this.$route.query.service,
+      serviceID: this.$route.query.sID,
     }
   },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.position-label {
+  text-transform: uppercase;
+}
+</style>
--- pages/people/index.vue
+++ pages/people/index.vue
@@ -3,19 +3,22 @@
     <section class="section-container">
       <breadcrumb current-page="People" />
       <paragraph
-        image="people.jpeg"
+        image="/people/people.jpeg"
         image-cap="A Plug-IT team at work around a desk"
         position="left"
       >
         <p>
-          <i>Plug-IT</i> is made up of talented individuals who share a
+          Plug-IT is made up of talented individuals who share a
           <strong>passion</strong>
           for achieving the best in everything they do. Our team puts
           <strong>our clients first</strong>, with a relentless focus on
           championing bold ideas that help them achieve the extraordinary.
-          <br />
+        </p>
+        <p>
           In every office, on every team, you’ll find passionate, collaborative
-          people who care for you and your success.
+          people who care for you and your success. We take a proactive approach
+          to helping you develop a strategy to address your goals and
+          objectives, using the most efficient methods available.
         </p>
       </paragraph>
     </section>
@@ -23,7 +26,6 @@
     <section class="section-container">
       <grid :elements="people" />
     </section>
-    <hr />
   </main>
 </template>
 
@@ -36,8 +38,16 @@ export default {
     Paragraph,
     Grid,
   },
-  async asyncData({ $axios }) {
+  async asyncData({ $axios, redirect }) {
+    // fetch the people from the database
     const { data } = await $axios.get(`${process.env.BASE_URL}/api/people`)
+    if (data === null || data.length === 0) {
+      return redirect(
+        '/error?err=Failed to retrieve employee data. Try again later.'
+      )
+    }
+
+    // convert the fetched data into the format required by the components
     const people = []
     data.forEach(function (person) {
       people.push({
